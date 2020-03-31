ee8afea688e8e60c212118abecbb5ed957950604
vanduc2514 <vanduc2514@gmail.com>
2020-03-31 15:41:00 +0700

    Refactor

--- src/AbsoluteNumberCalculator.java
+++ src/AbsoluteNumberCalculator.java
@@ -1,7 +1,11 @@
 public class AbsoluteNumberCalculator {
+
+    public static final int ZERO = 0;
+    public static final int NEGATIVE_ONE = -1;
+
     public static int findAbsolute(int number) {
-        if (number < 0) {
-            return number * -1;
+        if (number < ZERO) {
+            return number * NEGATIVE_ONE;
         }
         return number;
     }
